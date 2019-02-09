55618d0e1d58c24f57a2c42f80769f1f42f0e8f3
Robert Yuen <dungeonry@hotmail.com>
2019-02-09 04:45:13 -0800

    want to see if import works.

---
+++ backend/arbitrary.go
@@ -0,0 +1,6 @@
+package main
+
+type Arbitrary struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
--- backend/signup/function.go
+++ backend/signup/function.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+
+	"github.com/rhyuen/go-fs-lambda/backend/arbitrary"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Signup here.")
+	latest := arbitrary.Arbitrary{"A title", "A Content"}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(latest)
+
 }
